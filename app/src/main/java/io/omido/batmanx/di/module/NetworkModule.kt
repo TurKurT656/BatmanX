d3f1fb81ace8b5b8bdc8aa1a56bc6639953d5c6c
TurKurT656 <turkurt656@gmail.com>
2020-02-24 00:24:15 +0330

    added retrofit builder

--- app/src/main/java/io/omido/batmanx/di/module/NetworkModule.kt
+++ app/src/main/java/io/omido/batmanx/di/module/NetworkModule.kt
@@ -1,7 +1,19 @@
 package io.omido.batmanx.di.module
 
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+private const val BASE_URL = "http://www.omdbapi.com/"
 
 val networkModule = module {
+
+    single<Retrofit> {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(get())
+            .addConverterFactory(GsonConverterFactory.create(get()))
+            .build()
+    }
 
 }
