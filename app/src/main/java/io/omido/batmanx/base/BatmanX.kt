47af611ad23fc8fc8b169ff5809c78258aa835f2
TurKurT656 <turkurt656@gmail.com>
2020-02-23 23:16:29 +0330

    start koin

--- app/src/main/java/io/omido/batmanx/base/BatmanX.kt
+++ app/src/main/java/io/omido/batmanx/base/BatmanX.kt
@@ -2,6 +2,10 @@ package io.omido.batmanx.base
 
 import android.app.Application
 import io.omido.batmanx.BuildConfig
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 import timber.log.Timber
 
 /**
@@ -14,6 +18,16 @@ class BatmanX : Application() {
 
         // Plant Timber logger in
         if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+
+        // Initialize Koin DI
+        startKoin {
+            androidContext(this@BatmanX)
+            androidLogger(Level.DEBUG)
+            modules(
+                // List of modules goes here
+            )
+        }
+
 
     }
 
