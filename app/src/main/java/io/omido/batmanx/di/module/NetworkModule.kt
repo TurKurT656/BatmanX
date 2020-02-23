a0b228ebd4db264285d843e3ec77f38c507c4168
TurKurT656 <turkurt656@gmail.com>
2020-02-24 00:26:54 +0330

    added okhttp client

--- app/src/main/java/io/omido/batmanx/di/module/NetworkModule.kt
+++ app/src/main/java/io/omido/batmanx/di/module/NetworkModule.kt
@@ -1,12 +1,30 @@
 package io.omido.batmanx.di.module
 
+import io.omido.batmanx.BuildConfig
+import okhttp3.OkHttpClient
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 private const val BASE_URL = "http://www.omdbapi.com/"
 
+// Default okhttp timeout -> sec
+private const val TIMEOUT_DEBUG = 60L
+private const val TIMEOUT_RELEASE = 20L
+
 val networkModule = module {
+
+    single {
+        OkHttpClient.Builder().apply {
+            cache(get())
+            val timeout = if (BuildConfig.DEBUG) TIMEOUT_DEBUG else TIMEOUT_RELEASE
+            readTimeout(timeout, TimeUnit.SECONDS)
+            writeTimeout(timeout, TimeUnit.SECONDS)
+            connectTimeout(timeout, TimeUnit.SECONDS)
+        }
+            .build()
+    }
 
     single<Retrofit> {
         Retrofit.Builder()
