b416245f6e01d92847f68559a4de99a637911cbc
TurKurT656 <turkurt656@gmail.com>
2020-02-24 00:38:16 +0330

    added api key interceptor

---
+++ app/src/main/java/io/omido/batmanx/data/network/interceptor/ApiKeyInterceptor.kt
@@ -0,0 +1,25 @@
+package io.omido.batmanx.data.network.interceptor
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+
+private const val API_KEY = "apikey"
+private const val API_KEY_VALUE = "3e974fca"
+
+class ApiKeyInterceptor : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val original = chain.request()
+
+        val url = original.url.newBuilder()
+            .addQueryParameter(API_KEY, API_KEY_VALUE)
+            .build()
+
+        val request = original
+            .newBuilder()
+            .url(url)
+            .build()
+        return chain.proceed(request)
+    }
+}
