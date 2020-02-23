91fc2f647e6854dba9ac6e829441599f40666885
TurKurT656 <turkurt656@gmail.com>
2020-02-23 23:39:40 +0330

    added compat ktx fun to get drawable on context

--- app/src/main/java/io/omido/batmanx/util/ktx/Compat.kt
+++ app/src/main/java/io/omido/batmanx/util/ktx/Compat.kt
@@ -1,9 +1,16 @@
 package io.omido.batmanx.util.ktx
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import androidx.annotation.ColorRes
+import androidx.annotation.DrawableRes
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.content.ContextCompat
 
 fun Context.getColorCompat(@ColorRes colorRes: Int): Int {
     return ContextCompat.getColor(this, colorRes)
+}
+
+fun Context.getDrawableCompat(@DrawableRes drawableRes: Int): Drawable? {
+    return AppCompatResources.getDrawable(this, drawableRes)
 }
