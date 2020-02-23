457d8d999a9ca251b13c66f957a7dd77329c4d9d
TurKurT656 <turkurt656@gmail.com>
2020-02-23 23:44:17 +0330

    added some helper functions

--- app/src/main/java/io/omido/batmanx/base/BaseAndroidViewModel.kt
+++ app/src/main/java/io/omido/batmanx/base/BaseAndroidViewModel.kt
@@ -1,11 +1,28 @@
 package io.omido.batmanx.base
 
 import android.app.Application
+import android.graphics.drawable.Drawable
+import androidx.annotation.ColorRes
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import io.omido.batmanx.util.ktx.getColorCompat
+import io.omido.batmanx.util.ktx.getDrawableCompat
 
 abstract class BaseAndroidViewModel(
     application: Application
 ) : BaseViewModel() {
 
-    protected val batmanXApp = application as BatmanX
+    val batmanXApp = application as BatmanX
 
+    protected fun getString(@StringRes stringRes: Int, vararg formatArgs: Any?): String {
+        return batmanXApp.resources.getString(stringRes, *formatArgs)
+    }
+
+    protected fun getColor(@ColorRes colorRes: Int): Int {
+        return batmanXApp.applicationContext.getColorCompat(colorRes)
+    }
+
+    protected fun getDrawable(@DrawableRes drawableRes: Int): Drawable? {
+        return batmanXApp.applicationContext.getDrawableCompat(drawableRes)
+    }
 }
