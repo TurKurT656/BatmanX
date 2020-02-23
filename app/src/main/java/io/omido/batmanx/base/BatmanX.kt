d4cc9ce6b320f7b06a01710a49d9069c1bb521a8
TurKurT656 <turkurt656@gmail.com>
2020-02-23 23:14:32 +0330

    created custom application

---
+++ app/src/main/java/io/omido/batmanx/base/BatmanX.kt
@@ -0,0 +1,13 @@
+package io.omido.batmanx.base
+
+import android.app.Application
+import io.omido.batmanx.BuildConfig
+import timber.log.Timber
+
+/**
+ * Custom application class
+ */
+class BatmanX : Application() {
+
+
+}
